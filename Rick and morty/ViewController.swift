80661a9d2dd97444a0f04eb41cef899e0cc3a0ef
Natalia <nata22808@mail.ru>
2023-01-25 19:42:10 +0300

    Initial Commit

---
+++ Rick and morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and morty
+//
+//  Created by Наталья Миронова on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
